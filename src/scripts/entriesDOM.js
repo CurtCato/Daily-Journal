919a4e75ef3566068cf414cff201bc19745174b8
Curt Cato <ccato829@gmail.com>
2019-06-19 16:34:42 -0500

    cc-journal 6

--- src/scripts/entriesDOM.js
+++ src/scripts/entriesDOM.js
@@ -1,8 +1,9 @@
 const journalEntryContainer = document.querySelector("#entryLog");
 
 let renderJournalEntry = (arrayJournalEntry) => {
+  journalEntryContainer.innerHTML = ""
   arrayJournalEntry.forEach(function(journalEntry) {
-    let allJournalEntries = journalEntryComponent(journalEntry);
-    journalEntryContainer.innerHTML += allJournalEntries;
+    let newJournalEntry = journalEntryComponent(journalEntry);
+    journalEntryContainer.innerHTML += newJournalEntry;
   });
 };
